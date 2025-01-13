d7677e125b2603b1c3b500b5fc354262af2a1f6c
Kuba7331 <kuba@kamionka.eu>
2025-01-13 01:18:17 +0100

    small fixes

--- mulDivTest/divTest.asm
+++ mulDivTest/divTest.asm
@@ -7,8 +7,8 @@ read_number resd 1; 64-bits int = 8 bytes
 ; -- constants --
 section .data
 read_format db "%d", 0; the format string for scanf
-stringLiteral_0 db "Liczba nie jest podzielna przez 3", 0xd,0xa, 0
-stringLiteral_1 db "Liczba jest podzielna przez 3", 0
+stringLiteral_0 db "Nie jest", 0xd,0xa, 0
+stringLiteral_1 db "jest", 0
 ; -- Entry Point --
 section .text
 global main
@@ -29,7 +29,14 @@ main:
 	MOV rax, qword [read_number]
 	PUSH rax
 ; -- Place -- 
-	PUSH 3
+	PUSH 2
+; -- Div -- 
+	POP rbx
+	MOV rdx, 0
+	IDIV rbx
+	MOV qword [rsp], rax
+; -- Place -- 
+	PUSH 2
 ; -- Mod -- 
 	POP rbx
 	MOV rdx, 0
@@ -37,7 +44,7 @@ main:
 	MOV qword [rsp], rdx
 ; -- JmpEq0 -- 
 	CMP qword [rsp], 0
-	JE exit
+	JE yes
 ; -- Print -- 
 	sub rsp, 8
 	LEA rcx, stringLiteral_0
@@ -47,7 +54,7 @@ main:
 ; -- End -- 
 	JMP EXIT_LABEL
 ; -- Label -- 
-exit:
+yes:
 ; -- Print -- 
 	sub rsp, 8
 	LEA rcx, stringLiteral_1
---
+++ mulDivTest/divTest.psc
@@ -0,0 +1,12 @@
+scan
+place 2
+div           
+place 2
+mod          
+jmpEq0 yes    
+print "Nie jest"
+end
+
+yes:
+print "jest"
+end
